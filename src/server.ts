e391a7b75d5fa9d21874fbb3ff82b81e6080e6b2
Arison Firmino <arisonfirmino.dev@gmail.com>
2024-06-17 00:09:53 -0300

    chore: update server to use 0.0.0.0 as host for external access

--- src/server.ts
+++ src/server.ts
@@ -10,8 +10,10 @@ const start = async () => {
   });
   await app.register(routes);
 
+  const port = 3333;
+
   try {
-    await app.listen({ port: 3333 });
+    await app.listen({ port, host: "0.0.0.0" });
   } catch (error) {
     process.exit(1);
   }
